7dde493a6162213eb3688a43e67e6077a92532ff
ashishthomaschempolil <ashishthomas7@gmail.com>
2023-02-23 12:47:12 -0500

    fix: split the TaskStorage into taskmanager and taskstorage

--- src/task.js
+++ src/task.js
@@ -23,41 +23,71 @@ class Task {
 }
 
 /**
- * @class TaskStorage
- * @description This class is responsible for storing and retrieving tasks from the local storage
+ * @class TaskManager
+ * @description This class is responsible for managing the tasks
+ * @returns {object} - The task manager object
+ * Available properties:
+ * @property {array} tasks - The array of tasks
  * Available methods:
- * - saveTask
- * - getTaskById
- * - getAll
- * - updateTask
- * - deleteTask
+ * @method getTaskById - Get a task by id
+ * @method getAll - Get all tasks
+ * @method getTaskIndex - Get the index of a task
  */
-class TaskStorage {
+class TaskManager {
   constructor () {
     this.tasks = []
   }
 
-  saveTask (task) {
-    this.tasks.push(task)
-  }
-
-  getTaskById (taskid) {
-    return this.tasks.find(task => task.id === taskid)
+  getTaskById (taskId) {
+    return this.tasks.find(task => task.id === taskId)
   }
 
   getAll () {
     return this.tasks
   }
 
+  getTaskIndex (taskId) {
+    return this.tasks.findIndex(task => task.id === taskId)
+  }
+}
+
+/**
+ * @class TaskStorage
+ * @description This class is responsible for storing the tasks
+ * @returns {object} - The task storage object
+ * Available properties:
+ * @property {object} taskManager - The task manager object
+ * Available methods:
+ * @method saveTask - Save a task
+ * @method updateTask - Update a task
+ * @method deleteTask - Delete a task
+  */
+class TaskStorage {
+  constructor () {
+    this.taskManager = new TaskManager()
+  }
+
+  saveTask (task) {
+    if (task instanceof Task) {
+      // if task not in storage using taskid add it
+      if (!this.taskManager.getTaskById(task.id)) {
+        this.taskManager.tasks.push(task)
+      }
+    }
+  }
+
   updateTask (taskId, updatedTask) {
-    const index = this.tasks.findIndex(task => task.id === taskId)
-    if (index !== 1) {
-      this.tasks[index] = updatedTask
+    const index = this.taskManager.getTaskIndex(taskId)
+    if (index !== -1) {
+      this.taskmanager.tasks[index] = updatedTask
     }
   }
 
   deleteTask (taskId) {
-    this.tasks = this.tasks.filter(task => task.id !== taskId)
+    const index = this.taskManager.getTaskIndex(taskId)
+    if (index !== -1) {
+      this.taskManager.tasks.splice(index, 1)
+    }
   }
 }
 
