f5246c1085038d8478a49af5814f79a683ffbb77
ashishthomaschempolil <ashishthomas7@gmail.com>
2023-02-23 21:32:47 -0500

    fix: Styled sidebar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { Task, TaskStorage } from './task'
+import './style.css'
 
 
 const taskStorage = new TaskStorage()
@@ -17,5 +18,11 @@ console.log(taskStorage)
 const task2 = new Task('project2', 'title2', 'description2', 'dueDate2', 'priority2', 'notes2', 'checkList2')
 
 taskStorage.saveTask(task2)
+
+console.log(taskStorage)
+
+const task3 = new Task('project2', 'title3', 'description3', 'dueDate3', 'priority3', 'notes3', 'checkList3')
+
+taskStorage.saveTask(task3)
 
 console.log(taskStorage)
